7034989cdd0c85aebf31d6e261c51238b7f6b585
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2026-03-23 17:01:50 -0500

    Updated method signatures with typealias ClassificationResult

--- medspacy_pna/document_classification/document_classifier.py
+++ medspacy_pna/document_classification/document_classifier.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import Any
+from typing_extensions import Literal, TypeAlias
+
+ClassificationResult: TypeAlias = Literal["POS", "NEG", "POSSIBLE"]
+
 
 class BaseDocumentClassifier(ABC):
     """Abstract base class for pneumonia document classifiers.
     Classes inheriting from this will implement domain-specific logic for classifying documents.
     Each inheriting class should have a method _classify_document(doc, **kwargs) which returns a string.
     """
+
     name = "document_classifier"
     domain = None
 
@@ -16,20 +22,31 @@ class BaseDocumentClassifier(ABC):
         pass
 
     def __call__(self, doc, normalized=False, classification_schema=None, **kwargs):
-        doc._.document_classification = self.classify_document(doc, normalized=normalized, classification_schema=classification_schema, **kwargs)
+        doc._.document_classification = self.classify_document(
+            doc,
+            normalized=normalized,
+            classification_schema=classification_schema,
+            **kwargs,
+        )
         return doc
 
-    def classify_document(self, doc, normalized=False, classification_schema=None, **kwargs):
-        classification = self._classify_document(doc, classification_schema=classification_schema, **kwargs)
+    def classify_document(
+        self, doc, normalized=False, classification_schema=None, **kwargs
+    ) -> ClassificationResult:
+        classification = self._classify_document(
+            doc, classification_schema=classification_schema, **kwargs
+        )
         if normalized:
             classification = self.normalize_document_classification(classification)
         return classification
 
     @abstractmethod
-    def _classify_document(self, doc, classification_schema=None, **kwargs):
+    def _classify_document(
+        self, doc, classification_schema=None, **kwargs
+    ) -> ClassificationResult:
         pass
 
-    def normalize_document_classification(self, classification):
+    def normalize_document_classification(self, classification) -> ClassificationResult:
         if classification == "POSSIBLE":
             return "POS"
         return classification
