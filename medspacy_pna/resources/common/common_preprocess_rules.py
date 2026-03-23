b3a1ce504d2c786d55064d51d09d7d254d4dbddd
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2026-03-23 16:56:17 -0500

    Added type ignore; repl allows callable, but defaults to untyped "", causing typecheck error

--- medspacy_pna/resources/common/common_preprocess_rules.py
+++ medspacy_pna/resources/common/common_preprocess_rules.py
@@ -2,7 +2,9 @@ from medspacy.preprocess import PreprocessingRule
 import re
 
 preprocess_rules = [
-    PreprocessingRule(re.compile(r"([a-z/])([\r\n\s]{2,})([a-z])"),
-                      repl=lambda match: match.group(1) + " " + match.group(3),
-                      desc="Replace carriage returns in the middle of sentences with a single space"),
+    PreprocessingRule(
+        re.compile(r"([a-z/])([\r\n\s]{2,})([a-z])"),
+        repl=lambda match: match.group(1) + " " + match.group(3),  # type: ignore
+        desc="Replace carriage returns in the middle of sentences with a single space",
+    ),
 ]
