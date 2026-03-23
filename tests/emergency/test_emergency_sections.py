779ab0c827c9e2d17503c039d30a6a8ab0342619
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2026-03-23 17:50:07 -0500

    Added   # type: ignore

--- tests/emergency/test_emergency_sections.py
+++ tests/emergency/test_emergency_sections.py
@@ -6,37 +6,55 @@ from medspacy_pna.resources.clinical.clinical_postprocess_rules import RAD_TERMS
 
 nlp = build_nlp("emergency")
 
+
 class TestEmergencySections:
     def test_ap(self):
-        titles = ["A/P:", "Assessment/Plan:", "\nA:\n", "\nAssessment & Plan\n", "\nAssessment and Plan\n", "\nAssessment\n"]
+        titles = [
+            "A/P:",
+            "Assessment/Plan:",
+            "\nA:\n",
+            "\nAssessment & Plan\n",
+            "\nAssessment and Plan\n",
+            "\nAssessment\n",
+        ]
         template = "{} Pneumonia"
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == "observation_and_plan"
         # Now check ones that shouldn't match
-        titles = ["a:", "\na:", "a:\n", "\nAssessment & Plan", "Assessment and Plan\n", "Assessment pneumonia"]
+        titles = [
+            "a:",
+            "\na:",
+            "a:\n",
+            "\nAssessment & Plan",
+            "Assessment and Plan\n",
+            "Assessment pneumonia",
+        ]
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == None
 
     def test_mdm(self):
-        titles = ["MDM:", "Medical Decision Making:",]
+        titles = [
+            "MDM:",
+            "Medical Decision Making:",
+        ]
         template = "{} Pneumonia"
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == "medical_decision_making"
         # Now check ones that shouldn't match
         titles = []
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == None
 
     def test_disambiguate_impression(self):
@@ -49,14 +67,18 @@ class TestEmergencySections:
         for term in RAD_TERMS:
             text = template.format(term)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == "imaging"
 
         failed = []
-        ap_texts = ["Diagnostic Impressions: Pneumonia", "Impression: Pneumonia", "<<ADDENDUM>>: Pneumoia"]
+        ap_texts = [
+            "Diagnostic Impressions: Pneumonia",
+            "Impression: Pneumonia",
+            "<<ADDENDUM>>: Pneumoia",
+        ]
         for text in ap_texts:
             doc = nlp(text)
-            section = doc._.sections[-1]
+            section = doc._.sections[-1]  # type: ignore
             try:
                 assert section.category != "imaging"
             except AssertionError:
@@ -64,13 +86,17 @@ class TestEmergencySections:
         assert failed == []
 
         failed = []
-        imaging_texts = ["Labs and Studies: XYZ. Impression: Pneumonia", "Procedures: XYZ Impression: Pneumonia", "Imaging: XYZ Impression: Pneumonia",
-                         "Imaging: XYZ Addendum: Pneumonia", "Imaging: XYZ Impression: XYZ Addendum: Pneumonia",
-                         "Chest Xray - Impression: Pneumonia"
-                         ]
+        imaging_texts = [
+            "Labs and Studies: XYZ. Impression: Pneumonia",
+            "Procedures: XYZ Impression: Pneumonia",
+            "Imaging: XYZ Impression: Pneumonia",
+            "Imaging: XYZ Addendum: Pneumonia",
+            "Imaging: XYZ Impression: XYZ Addendum: Pneumonia",
+            "Chest Xray - Impression: Pneumonia",
+        ]
         for text in imaging_texts:
             doc = nlp(text)
-            section = doc._.sections[-1]
+            section = doc._.sections[-1]  # type: ignore
             try:
                 assert section.category == "imaging"
             except AssertionError:
@@ -83,7 +109,7 @@ class TestEmergencySections:
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == "hospital_course"
 
     def test_ed_course(self):
@@ -92,10 +118,10 @@ class TestEmergencySections:
         for title in titles:
             text = template.format(title)
             doc = nlp(text)
-            section = doc._.sections[0]
+            section = doc._.sections[0]  # type: ignore
             assert section.category == "ed_course"
 
     def test_appended_addendum(self):
         text = "<<ADDENDUM>>:\n\n"
         doc = nlp(text)
-        assert doc._.section_categories[0] == "addendum"
+        assert doc._.section_categories[0] == "addendum"  # type: ignore
